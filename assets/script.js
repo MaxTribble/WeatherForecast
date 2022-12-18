cd1f24007ce6d0141e5f21d81937898eed2cc1fc
Max Tribble <maxtribble@live.com>
2022-12-17 20:38:50 -0700

    saving searches to an area thye can be clicked and recalled

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,15 @@ var savedCities = document.querySelector("#savedCities")
 var search = document.querySelector("#search")
 var requestUrl = "https:api.openweathermap.org/data/2.5/forecast?lat=" + lat[0] + "&lon=" + lon[0] + "&units=imperial&cnt=5&appid=" + key;
 var coorUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + search.value + "&limit=1&appid=" + key
+
+//added the days of the week twice so the new date().getDay()+"i" can always find the right day
 var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var currentMonth = new Date().getMonth()+1
+
+//starting search
 search.value = "Denver"
+
+//pulling from the local save to populate the previous search area
 if(retreiveFromLocal !== null){
 for(i=0; i<retreiveFromLocal.length; i++){
   savingCity = document.createElement("li")
@@ -21,7 +27,8 @@ for(i=0; i<retreiveFromLocal.length; i++){
     savedCities.appendChild(savingCity)
 }}
 
-function test3(){
+//fetching the city name and extracting the coordinates then plugging them into the weather API
+function getWeather(){
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + search.value + "&limit=1&appid=" + key)
   .then(function (response) {
     if (response.status === 400) {
@@ -49,8 +56,7 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + search.value + "&limit
   .then(function (data) {
     console.log(data);
     console.log(data.list[1].main.temp)
-    document.getElementById("currentWeather").innerHTML = data.list[0].main.temp
-    document.getElementById("currentCity").innerHTML = search.value + " " + currentMonth + "/" + new Date().getDate() + "/" + new Date().getFullYear()
+    document.getElementById("currentCity").innerHTML = search.value + "<br>" + "<span class=currentMonth>" + currentMonth + "/" + new Date().getDate() + "/" + new Date().getFullYear() + "<br>" + "<br>" +  "Five Day Weather Forecast" + "</span>"
 
     function removeAllChildNodes(parent) {
       while (parent.firstChild) {
@@ -59,48 +65,50 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + search.value + "&limit
     removeAllChildNodes(document.querySelector("#forecast"))
     day1 = document.createElement("li")
     day1.classList.add("individualForecast")
-    day1.innerHTML = "Today" + "<br>" + "Temperature: " + data.list[0].main.temp + "<br>" + "WindSpeed: " + data.list[0].wind.speed + "<br>" + "Humidity: " + data.list[0].main.humidity + "%"
+    if(data.list[0].main.temp < 40){
+      var emoji = "&#x1F9CA"
+    } else if(data.list[0].main.temp < 60){
+      emoji = "&#9925"
+    } else { emoji = "&#x2600"}
+    day1.innerHTML = "Today " + "<span class=emoji>" + emoji + "</span>" + "<br>" + "Temperature: " + data.list[0].main.temp + "<br>" + "WindSpeed: " + data.list[0].wind.speed + "<br>" + "Humidity: " + data.list[0].main.humidity + "%"
     document.getElementById("forecast").appendChild(day1)
     for(i=1; i<data.list.length; i++){
+      if(data.list[0].main.temp < 40){
+        var emoji = "&#x1F9CA"
+      } else if(data.list[0].main.temp < 60){
+        emoji = "&#9925"
+      } else { emoji = "&#x2600"}
         day = document.createElement("li")
         day.classList.add("individualForecast")
-        day.innerHTML = dayOfWeek[new Date().getDay()+i] + "<br>" + "Temperature: " + data.list[i].main.temp + "<br>" + "WindSpeed: " + data.list[i].wind.speed + "<br>" + "Humidity: " + data.list[i].main.humidity + "%"
+        day.innerHTML = dayOfWeek[new Date().getDay()+i] + " <span class=emoji>" + emoji + "</span>" + "<br>" + "Temperature: " + data.list[i].main.temp + "<br>" + "WindSpeed: " + data.list[i].wind.speed + "<br>" + "Humidity: " + data.list[i].main.humidity + "%"
         document.getElementById("forecast").appendChild(day)
     }
  } )});
 };
+
+//calling the function to populate the page with Denver apon open
+getWeather()
+// saving the city to both the page, and locaal storage
 function saveCity(){
     savingCity = document.createElement("li")
     savingCity.innerHTML = search.value
     savingCity.classList.add("savedCity")
     savingCity.addEventListener("click", previousCity)
     localSaveCities.push(search.value)
-    console.log(localSaveCities)
     localStorage.setItem("cities", JSON.stringify(localSaveCities))
     savedCities.appendChild(savingCity)
  }
 
+ //when clicking a previous city, calls it to be searched
  function previousCity(){
   search.value=this.innerHTML
-  test3()
+  getWeather()
  }
 
-function functions(){
-  saveCity();
-  test3();
- }
-function test2(){
-    console.log(search.value)
-    console.log(coorUrl)
-    console.log("&#9925")
-    console.log(lon)
-    console.log(localSaveCities)
-    console.log(retreiveFromLocal)
-}
-document.getElementById("test2").innerHTML="&#9925"
-//&#x1F9CA
-//U+2600
-console.log(new Date().getMonth()+1)
-test3()
-document.getElementById("searchBtn").addEventListener("click", functions)
-document.getElementById("test2").addEventListener("click", test2)
+ //a bubbling function to call a city search and save the city name
+ function getAndLogWeather(){
+  getWeather()
+  saveCity()
+  }
+
+document.getElementById("searchBtn").addEventListener("click", getAndLogWeather)
